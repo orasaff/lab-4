be927a3e890d5975005926ff16ae8fd460cacd9e
Your Name <you@example.com>
2025-05-29 22:08:19 -0400

    tired task 3b but maybe doesn't work

--- src/main/java/api/MongoGradeDataBase.java
+++ src/main/java/api/MongoGradeDataBase.java
@@ -261,8 +261,27 @@ public class MongoGradeDataBase implements GradeDataBase {
 
         // TODO Task 3b: Implement the logic to get the team information
         // HINT: Look at the formTeam method to get an idea on how to parse the response
+        try {
+            response = client.newCall(request).execute();
+            responseBody = new JSONObject(response.body().string());
 
+            if (responseBody.getInt(STATUS_CODE) == SUCCESS_CODE) {
+                final JSONObject team = responseBody.getJSONObject("team");
+                final JSONArray membersArray = team.getJSONArray("members");
+                final String[] members = new String[membersArray.length()];
+                for (int i = 0; i < membersArray.length(); i++) {
+                    members[i] = membersArray.getString(i);
+                }
+                return new Team(NAME, members);
 
-        return null;
+            }
+            else {
+                throw new RuntimeException(responseBody.getString(MESSAGE));
+            }
+        }
+        catch (IOException | JSONException event) {
+            throw new RuntimeException(event);
+        }
+
     }
 }
--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -4,6 +4,8 @@ import api.GradeDataBase;
 import entity.Grade;
 import entity.Team;
 
+import java.util.ArrayList;
+
 /**
  * GetAverageGradeUseCase class.
  */
@@ -23,8 +25,11 @@ public final class GetAverageGradeUseCase {
         // Call the API to get usernames of all your team members
         float sum = 0;
         int count = 0;
+        // (complete) Task 3b: Go to the MongoGradeDataBase class and implement getMyTeam.
         final Team team = gradeDataBase.getMyTeam();
         // Call the API to get all the grades for the course for all your team members
+        // (completed) Task 3a: Complete the logic of calculating the average course grade for
+        //              your team members. Hint: the getGrades method might be useful.
         for (String username : team.getMembers()) {
             for (Grade grade : gradeDataBase.getGrades(username)) {
                 if (grade.getCourse().equals(course)) {
@@ -33,7 +38,6 @@ public final class GetAverageGradeUseCase {
                 }
             }
         }
-
         if (count == 0) {
             return 0;
         }
